518179099ca23857709dedaf6db443801d413ee5
Matthew Valentine <mattvalentine262@gmail.com>
2015-01-15 09:24:12 -0500

    Added timer and mine counter text
    functions get_time() and set_text() were added to manage the counter and
    timer functions shown at the top left and right of the window. They
    should properly stop and reset if you lose and reset the game.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import random
 import sys
 import math
+import time
 
 from pygame.locals import *
 
@@ -67,7 +68,7 @@ def right_click(coordinate):
         state[coordinate[0]][coordinate[1]][0] = 0
         remaining_mines += 1
         
-#Uncover a blank tile to expose a bomb or numbered tile    
+#Uncover a blank tile to expose a bomb or numbered tile.    
 def uncover_tile(coordinate):
     if(check_if_mine(coordinate)):
         GAME_SURFACE.blit(BOMB_TILE, get_pixel(coordinate))
@@ -95,15 +96,14 @@ def check_if_mine(coordinate):
     else:
         return False
 
+#Check to see if the use has won the game or not.
 def did_you_win():
     global won_game
     for x in range(X_TILES):
         for y in range(Y_TILES):
             if(state[x][y][0] is 0 and state[x][y][1] is not 10):
-                print("u didnt wen")
                 return
     won_game = True
-    print("u one")
     end_the_game()
         
 #Check all of the neighbors around the tile you left clicked on and count the mines.        
@@ -114,7 +114,8 @@ def check_neighbors(coordinate):
             if(check_if_mine((coordinate[0] + dx,coordinate[1] + dy))):
                 mine_counter += 1
     return mine_counter     
-    
+
+#Count the number of flags that are on adjacent tiles to the one you clicked.    
 def check_neighbors_flags(coordinate):
     flag_counter=0
     for dx, dy in [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i is j is 0)]: #iterate through adjacent cells
@@ -123,7 +124,7 @@ def check_neighbors_flags(coordinate):
                 flag_counter += 1
     return flag_counter     
 
-#propagates through a big empty pocket for the player
+#Propagates through a big empty pocket for the player.
 def propagate(coordinate):
     global state
     for dx, dy in [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i is j is 0)]: #iterate through adjacent cells
@@ -131,6 +132,7 @@ def propagate(coordinate):
             if(state[coordinate[0] + dx][coordinate[1] + dy][0] is 0):
                 left_click((coordinate[0] + dx,coordinate[1] + dy))
 
+#Click on all of the adjacent mines to blow them up.                
 def destroy_adjacent(coordinate):
     if(check_neighbors_flags(coordinate) is state[coordinate[0]][coordinate[1]][1]):
         for dx, dy in [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i is j is 0)]: #iterate through adjacent cells
@@ -157,7 +159,7 @@ def populate_state():
                 if(state[x][y][1] !=10):
                     state[x][y][1] = check_neighbors((x, y))
 
-#Uncover all bombs and lock the screen up if the game is lost                    
+#Uncover all bombs and lock the screen up if the game is lost.                    
 def end_the_game():
     if(lost_game):
         GAME_SURFACE.blit(DEAD_SMILEY, ((X_TILES*TILE_SIZE)/2 - SMILEY_SIZE/2, 0))
@@ -167,21 +169,48 @@ def end_the_game():
                     uncover_tile((x, y))
     elif(won_game):
         GAME_SURFACE.blit(COOL_SMILEY, ((X_TILES*TILE_SIZE)/2 - SMILEY_SIZE/2, 0))
-        
-                
-#Below are the initializations and main loop
 
+#Calculates the time to be displayed in the upper left hand corner.        
+def get_time():
+    global lost_game
+    time_dif = math.floor(time.time()-initial_time)
+    if(time_dif < 10):
+        return "00" + str(time_dif)
+    elif(time_dif < 100):
+        return "0" + str(time_dif)
+    elif(time_dif < 1000):
+        return str(time_dif)
+    else:
+        lost_game = true
+        end_the_game()
+        
+#Refresh the mine_counter and timer text boxes.
+def set_text():
+    timer = DIGITAL_FONT.render(get_time(), True, (255,0,0), (0, 0, 0))
+    mine_counter = DIGITAL_FONT.render(str(remaining_mines), True, (255,0,0), (0, 0, 0))
+    timer_box = timer.get_rect()
+    timer_box.topright = (X_TILES*TILE_SIZE,0)
+    mine_counter_box = mine_counter.get_rect()
+    mine_counter_box.topleft = (0,0)
+    if(not lost_game):
+        GAME_SURFACE.blit(timer, timer_box)
+        GAME_SURFACE.blit(mine_counter, mine_counter_box)
+    
+#Below are the initializations and main loop.
 remaining_mines = MINES
 pygame.init()
 pygame.display.set_caption('Minesweeper')
 FPSCLOCK = pygame.time.Clock()
+DIGITAL_FONT = pygame.font.Font('digital-7 (mono).ttf', 40)
+initial_time = 0
 
-#Initialize the board with blank tiles
+#Initialize the board with blank tiles and rest counters and timers.
 def reinit():
     global lost_game
     global won_game
     global remaining_mines
     global state
+    global initial_time
     for x in range(X_TILES):
         for y in range(Y_TILES):
             GAME_SURFACE.blit(NORMAL_TILE, (x*TILE_SIZE, y*TILE_SIZE + SMILEY_SIZE))
@@ -193,8 +222,9 @@ def reinit():
     populate_state()   
     GAME_SURFACE.blit(SMILEY, ((X_TILES*TILE_SIZE)/2 - SMILEY_SIZE/2, 0))
     remaining_mines = MINES
+    initial_time = time.time()
 
-#Below is the main loop
+#Below is the main loop.
 reinit()
 while True:
     for event in pygame.event.get():
@@ -204,7 +234,9 @@ while True:
         elif event.type is  MOUSEBUTTONDOWN and event.button is LEFT:
             left_click(get_tile(event.pos))
         elif event.type is  MOUSEBUTTONDOWN and event.button is RIGHT:
-            right_click(get_tile(event.pos))                
+            right_click(get_tile(event.pos))
+        
+    set_text()
     pygame.display.update() 
     FPSCLOCK.tick(FPS)           
 
